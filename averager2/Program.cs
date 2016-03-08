38be13d33ff3729234f976c2de3daa3923bdb42c
Mord Maman <m.maman@amaze.com>
2016-03-08 09:58:22 +0000

    m

---
+++ averager2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace averager2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = new List<double>();
+            bool go = true;
+            //fish
+            int i = 0;
+            double average;
+
+            while (go == true)
+            {
+                Console.WriteLine("Enter a number and press Enter to see the average (to stop type \"done\"): ");
+                string number = Console.ReadLine();
+
+                if (number.ToLower() == "done")
+                {
+                    go = false;
+                }
+                else
+                {
+                    try
+                    {
+                        double num = double.Parse(number);
+                        numbers.Add(num);
+                        i++;
+                        average = (numbers.Sum()) / i;
+                        Console.WriteLine("Your average is " + average.ToString("0.0"));
+                        continue;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("That is not a valid input");
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ averager2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("averager2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("averager2")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8caccf95-abe6-417d-86b4-2fe342d17574")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
